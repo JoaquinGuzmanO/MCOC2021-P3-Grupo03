5d1575fbe29fcccfed71620ff69eadb916112c3c
jaguzman <jaguzman1@miuandes.cl>
2021-11-16 20:26:36 -0300

    avance p3e4

---
+++ P3E3/P3E3 jaguzman/p3e3_grupo03_guzman.py
@@ -0,0 +1,48 @@
+import networkx as nx
+import osmnx as ox
+import matplotlib.pyplot as plt
+import geopandas as gps
+
+zonas_gdf = gps.read_file("eod.json")
+id_all_zonas = [293,292,296,295,303,305,269,265,270,322]
+id_zonas_interes_borde = [292,296,295,303,305,269,265,270,322]
+id_zona_central = 293
+all_zonas = zonas_gdf[zonas_gdf.ID.isin(id_all_zonas)]
+zonas_seleccionadas_borde = zonas_gdf[zonas_gdf.ID.isin(id_zonas_interes_borde)]
+zona_central = zonas_gdf[zonas_gdf.ID == id_zona_central]
+
+ox.config(use_cache=True)
+
+north = -33.405
+south = -33.445
+west = -70.580
+east = -70.535
+
+G = ox.graph_from_bbox(north, south, east, west, network_type="drive")
+gdf_nodes, gdf_edges = ox.graph_to_gdfs(G)
+
+gdf_cortado = gps.clip(gdf_edges, all_zonas)
+
+gdf_motorway = gdf_cortado[gdf_cortado.highway=="motorway"]
+gdf_secondary = gdf_cortado[gdf_cortado.highway=="secondary"]
+gdf_tertiary = gdf_cortado[gdf_cortado.highway=="tertiary"]
+gdf_primnary = gdf_cortado[gdf_cortado.highway=="primnary"]
+gdf_residential = gdf_cortado[gdf_cortado.highway=="residential"]
+
+                ####### GRAFICAR #######
+
+fig, ax = plt.subplots(1,1)
+zonas_seleccionadas_borde.plot(ax=ax,color="#CDCDCD")
+zona_central.plot(ax=ax,color="#FFB2B2")
+gdf_motorway.plot(ax=ax,color="red")
+gdf_secondary.plot(ax=ax,color="yellow")
+gdf_tertiary.plot(ax=ax,color="blue")
+gdf_primnary.plot(ax=ax,color="green")
+gdf_residential.plot(ax=ax,color="black")
+
+plt.title("Zona Las Condes: [293] vecinas: [292,296,295,303,305,269,265,270,322]\n")
+
+plt.show()
+
+
+
---
+++ P3E4/p3e4.py
@@ -0,0 +1,118 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+from networkx.algorithms import dijkstra_path
+
+def costo (ni,nf,attr):
+	funcosto_arco = attr["fcosto"]
+	flujo_arco = attr["flujo"]
+	return funcosto_arco(flujo_arco)
+
+
+			######################################
+			################ DATOS ###############
+			######################################
+
+G = nx.DiGraph()
+
+f1 = lambda f: 10 + f/120
+f2 = lambda f: 14 + 3*f/240
+f3 = lambda f: 10 + f/240
+
+OD = {
+	("A","C") : 1100, ("A","D") : 1100, ("A","E") : 1020,
+	("B","C") : 1140, ("B","D") : 1160,
+										("C","E") : 1170, ("C","G") : 1180,
+	("D","C") : 350, 					("D","E") : 1190, ("D","G") : 1200,
+}	
+
+OD_target = OD.copy()
+
+G.add_node("A", pos=[1,5])
+G.add_node("B", pos=[1,3])
+G.add_node("C", pos=[3,3])
+G.add_node("D", pos=[3,1])
+G.add_node("E", pos=[5,5])
+G.add_node("G", pos=[5,3])
+
+G.add_edge("A","B", fcosto=f1, flujo=0, costo=0, f="10 + f/120")
+G.add_edge("A","C", fcosto=f2, flujo=0, costo=0, f="14 + 3*f/240")
+G.add_edge("B","C", fcosto=f3, flujo=0, costo=0, f="10 + f/240")
+G.add_edge("B","D", fcosto=f2, flujo=0, costo=0, f="14 + 3*f/240")
+G.add_edge("C","E", fcosto=f2, flujo=0, costo=0, f="14 + 3*f/240")
+G.add_edge("C","G", fcosto=f3, flujo=0, costo=0, f="10 + f/240")
+G.add_edge("D","C", fcosto=f1, flujo=0, costo=0, f="10 + f/120")
+G.add_edge("D","G", fcosto=f2, flujo=0, costo=0, f="14 + 3*f/240")
+G.add_edge("G","E", fcosto=f1, flujo=0, costo=0, f="10 + f/120")
+
+
+			######################################
+			############# EQUILIBRIO #############
+			######################################
+
+while True:
+
+	se_asigno_demanda = False
+
+	for key in OD:
+		origen = key[0]
+		destino = key[1]
+		demanda_actual = OD[key]
+		demanda_target = OD_target[key]
+
+		if demanda_actual > 0:
+			path = dijkstra_path(G,origen,destino,weight=costo)
+			Nparadas = len(path)
+
+			for i_parada in range(Nparadas-1):
+				o = path[i_parada]
+				d = path[i_parada+1]
+				flujo_antes = G.edges[o,d]["flujo"]
+				G.edges[o,d]["flujo"] += 1
+
+			OD[key] -= 1
+			se_asigno_demanda = True
+
+	if not se_asigno_demanda:
+		break	
+
+for ni, nf in G.edges:
+	arco = G.edges[ni, nf]
+	funcosto_arco=arco["fcosto"]
+	flujo_arco=arco["flujo"]
+	arco["costo"] = funcosto_arco(flujo_arco)
+
+for i in G.edges():
+    G.edges[i]["flujo"] = round(G.edges[i]["flujo"],4)
+    G.edges[i]["costo"] = round(G.edges[i]["costo"],2)
+
+print(OD)
+print(OD_target)
+
+			######################################
+			############## GRAFICAR ##############
+			######################################
+
+pos = nx.get_node_attributes(G,"pos")
+
+plt.figure(1)
+plt.title("Flujo final en cada arco")
+labels = nx.get_edge_attributes(G,"flujo")
+ax1 =plt.subplot(111)
+nx.draw(G,pos,with_labels=True,font_weight="bold")
+nx.draw_networkx_edge_labels(G,pos,edge_labels=labels)
+
+plt.figure(2)
+plt.title("Costo final en cada arco")
+labels = nx.get_edge_attributes(G,"costo")
+ax1 =plt.subplot(111)
+nx.draw(G,pos,with_labels=True,font_weight="bold")
+nx.draw_networkx_edge_labels(G,pos,edge_labels=labels)
+
+plt.figure()
+plt.title("Funciones de costo de cada arco")
+labels = nx.get_edge_attributes(G,"f")
+ax1 =plt.subplot(111)
+nx.draw(G,pos,with_labels=True,font_weight="bold")
+nx.draw_networkx_edge_labels(G,pos,edge_labels=labels)
+
+plt.show()
