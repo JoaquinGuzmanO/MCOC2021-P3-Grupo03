0e0e39f28a636f7402f8c2acc5c7721c3223f057
jaguzman <jaguzman1@miuandes.cl>
2021-11-20 17:09:38 -0300

    p3e5 final}

--- P3E5/p3e5.py
+++ P3E5/p3e5.py
@@ -30,20 +30,24 @@ zonas_destino_AVO = zonas_gdf[zonas_gdf.ID.isin(list(set(id_zonas_destino_AVO)))
 all_id = id_zonas_destino_AVO + id_zonas_AVO + id_zonas_cercanas_AVO
 all_id = list(set(all_id))
 all_id.pop(all_id.index(816))
+all_id.pop(all_id.index(827))
+all_id.pop(all_id.index(727))
+all_id.pop(all_id.index(754))
+all_id.pop(all_id.index(328))
 all_zonas = zonas_gdf[zonas_gdf.ID.isin(all_id)]
-
+# centroid = all_zonas.centroid
 ox.config(use_cache=True)
 
 north = -33.34
-south = -33.8
-west = -71.1
-east = -70.2
+south = -33.655
+west = -70.9
+east = -70.45
 
 G = ox.graph_from_bbox(north, south, east, west, network_type="drive")
 gdf_nodes, gdf_edges = ox.graph_to_gdfs(G)
 
-gdf_cortado = gps.clip(gdf_edges, zonas_AVO)
-gdf_cortado_AVO = gps.clip(gdf_edges, zonas_AVO)
+gdf_cortado = gps.overlay(gdf_edges, all_zonas,"intersection")
+gdf_cortado_AVO = gps.overlay(gdf_edges, zonas_AVO,"intersection")
 
 gdf_motorway = gdf_cortado[gdf_cortado.highway=="motorway"]
 gdf_secondary = gdf_cortado[gdf_cortado.highway=="secondary"]
@@ -54,16 +58,18 @@ gdf_AVO2 = gdf_cortado_AVO[gdf_cortado_AVO.name=="Avenida Américo Vespucio Sur"
 gdf_AVO3 = gdf_cortado_AVO[gdf_cortado_AVO.name=="Avenida Américo Vespucio Norte"]
 gdf_AVO4 = gdf_cortado_AVO[gdf_cortado_AVO.name=="Avenida Américo Vespucio"]
 
-                ####### GRAFICAR #######
+#                 ####### GRAFICAR #######
 
 fig, ax = plt.subplots(1,1)
 
 all_zonas.plot(ax=ax,color="#CDCDCD")
-
-gdf_motorway.plot(ax=ax,color="orange")
-gdf_primary.plot(ax=ax,color="yellow")
-gdf_secondary.plot(ax=ax,color="green")
-gdf_tertiary.plot(ax=ax,color="blue")
+# for idx,row in all_zonas.iterrows():
+#     c=row.geometry.centroid
+#     ax.annotate(text=row["ID"],xy=(c.x,c.y),horizontalalignment="center")
+gdf_motorway.plot(ax=ax,color="orange",width=1)
+gdf_primary.plot(ax=ax,color="yellow",width=0.8)
+gdf_secondary.plot(ax=ax,color="green",width=0.5)
+gdf_tertiary.plot(ax=ax,color="blue",width=1)
 
 gdf_AVO1.plot(ax=ax,color="red")
 gdf_AVO2.plot(ax=ax,color="red")
